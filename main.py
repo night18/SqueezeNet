056ac5d0c22c4087ea301ff911cd40a766f6ab99
night18 <warrior.sp@gmail.com>
2019-02-24 17:43:13 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,77 @@
+'''
+=======================================================================================
+Author: Chun-Wei Chiang
+Date: 2019.02.24
+Description: Train SqueezeNet network
+=======================================================================================
+'''
+import tensorflow as tf
+import model
+import util
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+cifar_10_dir = "cifar-10"
+validation_number = 5000
+train_number = 50000 - validation_number
+epochs = 10
+
+def testModel(model, x_test, y_test, learning_rate):
+	score = model.evaluate(x_test, y_test)
+	print("=============================================")
+	print("Test perforemance of learning rate " + str(learning_rate))
+	print('Test loss:'+ str(score[0]))
+	print('Test accuracy:'+ str(score[1]))
+
+
+def plot_performance(histories, name_list, isloss = True, isVal = False, isBoth = False):
+	#isloss means whether plot loss. If True, plot loss, nor plot accuracy
+
+	perforemance = 'loss' if isloss else 'acc'
+
+	# print(perforemance)
+	fig = plt.figure()
+
+	for hist in histories:
+		if isBoth:
+			plt.plot(hist[perforemance])
+			plt.plot(hist['val_' + perforemance])
+		else:
+			val = 'val_' if isVal else ''
+			perforemance = val + perforemance
+			plt.plot(hist[perforemance])
+
+	plt.xticks(np.arange(0, epochs +1 , epochs/5 ))
+	plt.ylabel(perforemance)
+	plt.xlabel( "epochs" )
+	plt.legend( name_list , loc=0)
+	plt.show()
+	fig.savefig(perforemance + '.png')
+
+
+if __name__ == '__main__':
+	train_data, _, train_labels, test_data, _, test_labels, label_names = util.loadCIFAR10(cifar_10_dir)
+
+	train_data, validation_data = train_data[ 0:train_number ], train_data[ train_number:50000 ]
+	train_labels, validation_labels = train_labels[ 0:train_number ], train_labels[ train_number:50000 ]
+	train_data, validation_data, test_data = train_data/255.0, validation_data/255.0 , test_data/255.0
+	
+	train_labels= to_categorical(train_labels,num_classes=10)
+	validation_labels = to_categorical(validation_labels, num_classes=10)
+	test_labels= to_categorical(test_labels,num_classes=10)
+
+	learning_rate_list = [0.01, 0.001, 0.0001]
+	histories = []
+	name_list = []
+
+	for x in learning_rate_list:
+		squeezenet = model.trainModel(train_data, train_labels, validation_data, validation_labels, epochs=epochs, learning_rate=x)
+		testModel(squeezenet, test_data, test_labels, x)
+		path = "history/squeezeNet_{}".format(x)
+		histories.append( util.unpickle(path) )
+		name = "{}".format(x)
+		name_list.append( name )
+		name_list.append( name + "_val" )
+
+	plot_performance(histories, name_list, isloss = True, isBoth = True)
---
+++ model.py
@@ -0,0 +1,113 @@
+'''
+=======================================================================================
+Author: Chun-Wei Chiang
+Date: 2019.02.24
+Description: Train SqueezeNet network
+=======================================================================================
+'''
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, Activation, concatenate, MaxPool2D, Flatten, Dense
+from tensorflow.keras.models import Model, save_model, load_model
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import ModelCheckpoint
+import pickle
+
+sqz = 'sqz1'
+exp = 'exp'
+exp1 = 'exp1'
+exp3 = 'exp3'
+relu = 'relu_'
+models_dir = "models"
+history_dir = "history"
+checkpoint_dir = "checkpoint"
+
+
+def fire_module(prv_lyr, fire_id, squeeze = 3, expand = 4):
+	s_id = 'fire' + str(fire_id) + '/'
+
+	#squeeze layer
+	sqz_layer = Conv2D( squeeze, kernel_size=(1,1), padding='same', name=s_id+sqz )(prv_lyr)
+	sqz_layer = Activation( 'relu', name=s_id+relu+sqz )(sqz_layer)
+
+	#expand layer
+	#1*1
+	exp1_layer = Conv2D( expand, kernel_size=(1,1), padding='same', name=s_id+exp1)(sqz_layer)
+	exp1_layer = Activation( 'relu', name=s_id+relu+exp1)(exp1_layer)
+	#3*3
+	exp3_layer = Conv2D( expand, kernel_size=(3,3), padding='same', name=s_id+exp3)(sqz_layer)
+	exp3_layer = Activation( 'relu', name=s_id+relu+exp3)(exp3_layer)
+
+	cnct_layer = concatenate([exp1_layer, exp3_layer])
+
+	return cnct_layer
+
+def squeezeNet(input_img):
+	inputs = Input(shape=(32,32,3))
+
+	x = Conv2D(96, kernel_size=(4,4), padding='same', name='conv1' )(inputs)
+	x = Activation('relu', name='relu_conv1')(x)
+	x = MaxPool2D(pool_size=(3,3), strides=(2,2), name='pool1')(x)
+
+	x = fire_module(x, fire_id=2)
+	x = fire_module(x, fire_id=3)
+	x = fire_module(x, fire_id=4)		
+	x = MaxPool2D(pool_size=(3,3), strides=(2,2), name='pool2')(x)
+
+	x = fire_module(x, fire_id=5)
+	x = fire_module(x, fire_id=6)
+	x = fire_module(x, fire_id=7)
+	x = fire_module(x, fire_id=8)
+	x = MaxPool2D(pool_size=(3,3), strides=(2,2), name='pool3')(x)
+
+	x = fire_module(x, fire_id=9)
+	x = Conv2D(10, kernel_size=(4,4), padding='same', name='conv10')(x)
+	x = Activation('relu', name='relu_conv10')(x)
+
+	x = Flatten()(x)
+	x = Dense(10)(x)
+	x = Activation('softmax', name='softmax')(x)
+
+	model = Model(inputs, x, name='squeezeNet')
+
+	return model
+
+def trainModel(train_data, train_label, validation_data, validation_label, epochs=200, learning_rate=0.001 ):
+	model = None
+	h5_storage_path = models_dir + "/" + "squeezeNet_" + str(learning_rate) + ".h5"
+	hist_storage_path = history_dir + "/" + "squeezeNet_" + str(learning_rate)
+	checkpoint_path = checkpoint_dir + "/" + "squeezeNet_" + str(learning_rate) + ".hdf5"
+
+	model = squeezeNet(train_data)
+	model.compile(loss = tf.keras.losses.categorical_crossentropy,
+						optimizer = SGD(lr = learning_rate),
+						metrics = ['accuracy'])
+
+	checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+	callbacks_list = [checkpoint]
+
+	#Fit the model
+	hist = model.fit(
+		train_data,
+		train_label,
+		epochs = epochs,
+		batch_size = 64,
+		validation_data=(validation_data, validation_label),
+		callbacks=callbacks_list,
+		verbose= 1)
+
+	#save the model
+	save_model(
+		model,
+		h5_storage_path,
+		overwrite=True,
+		include_optimizer=True
+	)
+
+	#Save the history of training
+	with open(hist_storage_path, 'wb') as file_hist:
+		pickle.dump(hist.history, file_hist)
+
+	print("Successfully save the model at " + h5_storage_path)
+
+	return model
